27f3f9e3ccae525d82cde28e01aa243d7f5ff536
Enidbii <chebsenid@gmail.com>
2023-03-23 07:34:30 -0700

    okay monty

---
+++ main.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * main - monty code interpreter
+ * @argc: no of arguments
+ * @argv: pointer to args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int count = 0;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[i], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		count++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
+	}
+	free_stack(stack);
+	fclose(file);
+	return (0);
+}
